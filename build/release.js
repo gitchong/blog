4f30cc6f8e4e65b60a8b11e13aacee3dff369645
金洋 <liujinyang@bigtiger.me>
2017-05-08 13:39:19 +0800

    add create posts json v1.0.0 code

---
+++ build/release.js
@@ -0,0 +1,27 @@
+require('./create-posts-json')('./posts');
+var fs = require('fs');
+var path = require('path');
+var Client = require('ftp');
+var async = require('async');
+var client = new Client();
+
+client.on('ready', function() {
+  const paths = fs.readdirSync(path.join(__dirname, '..', 'static', 'posts'));
+  const upload = function (file, callback) {
+    client.put(path.join(__dirname, '..', 'static', 'posts', file), `./htdocs/static/posts/${file}`, callback);
+  };
+
+  async.map(paths, upload, function (err) {
+    if (err) {
+      return console.error(err);
+    } else {
+      client.end();
+    }
+  });
+});
+
+client.connect({
+  host: '域名',
+  user: '账号',
+  password: '密码',
+});
