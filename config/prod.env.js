49e0dc2d5a1edf77ff212af5144c91d9e5726c0a
金洋 <liujinyang@bigtiger.me>
2017-05-09 12:32:00 +0800

    add create posts json v1.0.0 code

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   WEBSITE_TITLE: '"BIGTIGER.ME"', // 网站的title
-  FTP_HOST: '""', // ftp地址
-  FTP_USER: '""', // ftp用户名
-  FTP_PASSWORD: '""' //ftp密码
+  FTP_HOST: '', // ftp地址
+  FTP_USER: '', // ftp用户名
+  FTP_PASSWORD: '' //ftp密码
 }
